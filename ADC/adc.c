6472f604011894105f1dd396df96a95fee0d66a0
nicozorza <nico.zorzano@live.com.ar>
2017-04-16 16:56:43 -0300

    Estuve probando de plotear los datos del puerto serie, pero tengo que encontrar la forma de mandar en formato ASCII en lugar de binario

--- ADC/adc.c
+++ ADC/adc.c
@@ -1,25 +1,44 @@
 #include "adc.h"
 
-
-volatile int aux;
+volatile int aux=0;
 volatile uint16_t convertion;
-ISR(ADC_vect){
-/*	if( read_button() == FALSE )*/
-	if( aux%1000 == 0 ){
-		toggle_led1();
+
+int char_to_int( uint8_t num ){
+
+	int aux=0;
 	
-		if((convertion=ADC_get_convertion()) == (0xFF))
-			toggle_led2();
-	}
-	
-	ADC_start_convertion();
-	aux++;
+	aux+=num&(1<<0);
+	aux+=(num&(1<<1))*2;
+	aux+=(num&(1<<2))*4;
+	aux+=(num&(1<<3))*8;
+	aux+=(num&(1<<4))*16;
+	aux+=(num&(1<<5))*32;
+	aux+=(num&(1<<6))*64;
+	aux+=(num&(1<<7))*128;
+return aux;
 }
 
 
+ISR(ADC_vect){
+	
+	int aux;
+	convertion=ADC_get_convertion();
+	aux=char_to_int( convertion&0x0F );
+	USART_transmit( aux>>8 );
+	USART_transmit( aux&0x0F );
+	USART_transmit( '\r' );
+	USART_transmit( '\n' );
+/*	USART_transmit( '\0' );*/
+	ADC_start_convertion();
+	aux++;
+	
+	if( aux%100 == 0)
+		toggle_led1();
+}
+
 int main( void ){
 	init_leds( (1<<LED1)|(1<<LED2) , 0 );
-	init_button();
+	USART_init();
 	init_ADC(0x00);	//Init AD0
 	
 	ADC_start_convertion();
